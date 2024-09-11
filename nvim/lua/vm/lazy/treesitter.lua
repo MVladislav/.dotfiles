3ff480610da6faa9385d28a6f16bc86dd878d3cf
MVladislav <dev@mvladislav.online>
2024-09-11 22:05:26 +0200

    refactor: nvim update treesitter with fold function

--- nvim/lua/vm/lazy/treesitter.lua
+++ nvim/lua/vm/lazy/treesitter.lua
@@ -50,6 +50,37 @@ return {
         },
       }
       vim.treesitter.language.register('templ', 'templ')
+
+      vim.opt.foldmethod = "expr"
+      vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+      vim.opt.foldcolumn = "0"
+      vim.opt.foldtext = ""
+      vim.opt.foldlevel = 99
+      vim.opt.foldlevelstart = 4
+      vim.opt.foldnestmax = 4
+
+      -- Function to set fold level and fold blocks
+      local function FoldToLevel(level)
+        -- Close all folds first (zM), then set the fold level
+        vim.cmd("normal! zM")  -- Close all folds
+        vim.opt.foldlevel = level
+        -- Open folds up to the specific level (e.g., z1, z2, etc.)
+        vim.cmd("normal! z" .. level)
+      end
+
+      -- Map Ctrl+k Ctrl+0 to fold unfold
+      vim.keymap.set("n", "<A-k><A-0>", function() vim.cmd("normal! zR") end)
+      -- Map Ctrl+k Ctrl+1 to fold depth 1
+      vim.keymap.set("n", "<A-k><A-1>", function() FoldToLevel(0) end)
+      -- Map Ctrl+k Ctrl+2 to fold depth 2
+      vim.keymap.set("n", "<A-k><A-2>", function() FoldToLevel(1) end)
+      -- Map Ctrl+k Ctrl+3 to fold depth 3
+      vim.keymap.set("n", "<A-k><A-3>", function() FoldToLevel(2) end)
+      -- Map Ctrl+k Ctrl+4 to fold depth 4
+      vim.keymap.set("n", "<A-k><A-4>", function() FoldToLevel(4) end)
+      -- Map Ctrl+k Ctrl+5 to fold depth 5
+      vim.keymap.set("n", "<A-k><A-5>", function() FoldToLevel(5) end)
+
     end
   }
 }
