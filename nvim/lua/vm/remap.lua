0cb0cb08392241c00db311786bba60a77163fa4c
MVladislav <dev@mvladislav.online>
2025-09-21 20:10:14 +0200

    chore: nvim cleanup (remap + set)

--- nvim/lua/vm/remap.lua
+++ nvim/lua/vm/remap.lua
@@ -1,79 +1,89 @@
+-- Keymaps configuration
 local keymap = vim.keymap
 
+-- Leader keys
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- File navigation
+keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open netrw file explorer" })
 
 -- Move selected lines up/down
-keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
 
--- Combine current line with line below
-keymap.set("n", "J", "mzJ`z")
+-- Join current line with the one below, keeping cursor in place
+keymap.set("n", "J", "mzJ`z", { desc = "Join line below (keep cursor)" })
 
--- Cursor jump centered
-keymap.set("n", "<C-d>", "<C-d>zz")
-keymap.set("n", "<C-u>", "<C-u>zz")
--- Search next/prev
-keymap.set("n", "n", "nzzzv")
-keymap.set("n", "N", "Nzzzv")
+-- Cursor jumps (keep centered)
+keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Page down (centered)" })
+keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Page up (centered)" })
 
--- greatest remap ever
-keymap.set("x", "<leader>p", [["_dP]])
+-- Search navigation (keep centered)
+keymap.set("n", "n", "nzzzv", { desc = "Next search result (centered)" })
+keymap.set("n", "N", "Nzzzv", { desc = "Prev search result (centered)" })
 
--- Copy modes
-keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-keymap.set("n", "<leader>Y", [["+Y]])
+-- Paste without overwriting register
+keymap.set("x", "<leader>p", [["_dP]], { desc = "Paste without yanking" })
 
-keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+-- Copy (yank) to system clipboard
+keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank to system clipboard" })
+keymap.set("n", "<leader>Y", [["+Y]], { desc = "Yank line to system clipboard" })
 
--- This is going to get me cancelled
-keymap.set("i", "<C-c>", "<Esc>")
-keymap.set("n", "Q", "<nop>")
+-- Delete without affecting register
+keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = "Delete without yanking" })
 
--- New tmux session with fzf search
-keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+-- Quality-of-life mappings
+keymap.set("i", "<C-c>", "<Esc>", { desc = "Escape insert mode" })
+keymap.set("n", "Q", "<nop>", { desc = "Disable Q (Ex mode)" })
 
--- Format with lsp
-keymap.set("n", "<leader>f", vim.lsp.buf.format)
+-- New tmux session with fzf search (safe check for tmux)
+keymap.set("n", "<C-f>", function()
+  if vim.fn.exists("$TMUX") == 1 then
+    vim.cmd("silent !tmux neww tmux-sessionizer")
+  else
+    print("Not running inside tmux")
+  end
+end, { desc = "Open tmux sessionizer" })
 
-keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+-- Format with LSP
+keymap.set("n", "<leader>f", function()
+  vim.lsp.buf.format({ async = true })
+end, { desc = "Format buffer with LSP" })
 
-keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+-- Quickfix and location list navigation
+keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz", { desc = "Next quickfix item" })
+keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz", { desc = "Prev quickfix item" })
+keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz", { desc = "Next location list item" })
+keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz", { desc = "Prev location list item" })
 
-keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+-- Search & replace word under cursor
+keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+  { desc = "Search & replace word under cursor" }
+)
 
-local function vimgrep_current_word()
-  -- Get the current word under the cursor
+-- Make current file executable
+keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true, desc = "Make file executable" })
+
+-- Clear search highlight
+keymap.set("n", "<leader>h", ":nohlsearch<CR>", { desc = "Clear search highlight" })
+
+-- Replace word in project
+local function vimgrep_replace_all()
   local current_word = vim.fn.expand("<cword>")
-  -- Run vimgrep with the current word in all files in the current directory
   vim.cmd("vimgrep /" .. current_word .. "/ `find . -type f`")
-  -- Open the quickfix list
   vim.cmd("copen")
-
-  -- Prompt the user for the new word
   local new_word = vim.fn.input("Replace with: ")
-  -- Run :cdo command to replace current_word with new_word
   vim.cmd("cdo %s/" .. current_word .. "/" .. new_word .. "/gc")
 end
-keymap.set("n", "<leader>rnl", vimgrep_current_word, { silent = true })
+keymap.set("n", "<leader>rnp", vimgrep_replace_all, { silent = true, desc = "Replace word in project" })
 
-local function vimgrep_current_word()
-  -- Get the current word under the cursor
+-- Replace word in current file
+local function vimgrep_replace_file()
   local current_word = vim.fn.expand("<cword>")
-  -- Run vimgrep with the current word in all files in the current directory
   vim.cmd("vimgrep /" .. current_word .. "/ %")
-  -- Open the quickfix list
   vim.cmd("copen")
-
-  -- Prompt the user for the new word
   local new_word = vim.fn.input("Replace with: ")
-  -- Run :cdo command to replace current_word with new_word
   vim.cmd("cdo %s/" .. current_word .. "/" .. new_word .. "/gc")
 end
-keymap.set("n", "<leader>rnf", vimgrep_current_word, { silent = true })
+keymap.set("n", "<leader>rnf", vimgrep_replace_file, { silent = true, desc = "Replace word in file" })
--- nvim/lua/vm/set.lua
+++ nvim/lua/vm/set.lua
@@ -1,86 +1,60 @@
-vim.cmd("let g:netrw_liststyle = 3")
+-- Better netrw
+vim.g.netrw_liststyle = 3
 
+-- UI
 vim.opt.background = "dark"
-vim.opt.guicursor = ""
--- vim.opt.guicursor = {
---   "n-v-c:block",                                 -- Normal, visual, command-line: block cursor
---   "i-ci-ve:ver25",                               -- Insert, command-line insert, visual-exclude: vertical bar cursor with 25% width
---   "r-cr:hor20",                                  -- Replace, command-line replace: horizontal bar cursor with 20% height
---   "o:hor50",                                     -- Operator-pending: horizontal bar cursor with 50% height
---   "a:blinkwait700-blinkoff400-blinkon250",       -- All modes: blinking settings
---   "sm:block-blinkwait175-blinkoff150-blinkon175", -- Showmatch: block cursor with specific blinking settings
--- }
-
--- Enable relative line numbers
-vim.opt.nu = true
-vim.opt.rnu = true
+vim.opt.guicursor = "" -- stable cursor in all modes
+vim.opt.number = true
 vim.opt.relativenumber = true
-
--- Set tabs to 2 spaces
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.expandtab = true
-
--- Enable auto indenting and set it to spaces
-vim.opt.shiftwidth = 2
-vim.opt.smartindent = true
-
--- Enable smart indenting (see https://stackoverflow.com/questions/1204149/smart-wrap-in-vim)
-vim.opt.breakindent = true
-
-vim.opt.wrap = false
-
-vim.opt.swapfile = false
-vim.opt.backup = false
--- Enable persistent undo history
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
-
--- incremental searching
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
-
--- Enable 24-bit color
-vim.opt.termguicolors = true
-
--- Always keep 8 lines above/below cursor unless at start/end of file
-vim.opt.scrolloff = 8
--- Enable the sign column to prevent the screen from jumping
 vim.opt.signcolumn = "yes"
-
-vim.opt.isfname:append("@-@")
-
--- Decrease updatetime to 200ms
-vim.opt.updatetime = 50
-
--- Place a column line
+vim.opt.cursorline = true
 vim.opt.colorcolumn = "130"
-
--- Better splitting
+vim.opt.termguicolors = true
+vim.opt.mouse = "a"
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 
--- Enable mouse mode
-vim.opt.mouse = "a"
+-- Tabs & indent
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.breakindent = true
+vim.opt.wrap = false
 
--- Enable ignorecase + smartcase for better searching
+-- Undo & backup
+vim.opt.swapfile = false
+vim.opt.backup = false
+local undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.fn.mkdir(undodir, "p")
+vim.opt.undodir = undodir
+vim.opt.undofile = true
+
+-- Search
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
 
--- Set completeopt to have a better completion experience
+-- Scrolling
+vim.opt.scrolloff = 8
+
+-- Performance
+vim.opt.updatetime = 200 -- more responsive LSP, default=4000ms
+
+-- Completion
 vim.opt.completeopt = { "menuone", "noselect" }
 
--- Enable cursor line highlight
-vim.opt.cursorline = true
-
--- Set fold settings
--- These options were recommended by nvim-ufo
--- See: https://github.com/kevinhwang91/nvim-ufo#minimal-configuration
-vim.opt.foldcolumn = "0"
+-- Folds (for nvim-ufo)
+vim.opt.foldcolumn = "1"
 vim.opt.foldlevel = 99
 vim.opt.foldlevelstart = 99
 vim.opt.foldenable = true
 
-vim.opt.clipboard = 'unnamedplus'
-
+-- Misc
+vim.opt.isfname:append("@-@")
 vim.opt.backspace = "indent,eol,start"
+if vim.fn.has("clipboard") == 1 then
+  vim.opt.clipboard = "unnamedplus"
+end
